07830212da654cd6a01e8473c08eb510034b4547
MrSpaar <pistre.julien@proton.me>
2023-07-11 19:46:31 +0200

    Improved installer compatibility

--- install.sh
+++ install.sh
@@ -2,18 +2,25 @@ NC='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-if [ -f "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
-    printf "${GREEN}Installing dependencies for debian based distro${NC}\n"
-    sudo apt install cmake make g++ libgtk-3-dev libsqlite3-dev || printf "${RED}Could not install dependencies${NC}\n" || exit 1
-elif [ -f "/etc/arch-release" ]; then
-    printf "${GREEN}Installing dependencies for arch based distro${NC}\n"
-    sudo pacman -S cmake make gcc gtkmm3 libsqlite-devel || printf "${RED}Could not install dependencies${NC}\n" || exit 1
-elif [ -f "/etc/fedora-release" ]; then
-    printf "${GREEN}Installing dependencies for fedora based distro${NC}\n"
-    sudo dnf install cmake make gcc-g++ gtkmm3.0-devel sqlite-devel || printf "${RED}Could not install dependencies${NC}\n" || exit 1
+if [ -x "$(command -v apt)" ]; then
+  printf "${GREEN}Installing dependencies for debian based distro${NC}\n"
+  sudo apt update || printf "${RED}Could not update package list${NC}\n" || exit 1
+  sudo apt install cmake make g++ libgtk-3-dev libsqlite3-dev || printf "${RED}Could not install dependencies${NC}\n" || exit 1
+elif [ -x "$(command -v dnf)" ]; then
+  printf "${GREEN}Installing dependencies for rpm based distro${NC}\n"
+  sudo dnf update || printf "${RED}Could not update package list${NC}\n" || exit 1
+  sudo dnf install cmake make gcc-g++ gtkmm3.0-devel sqlite-devel || printf "${RED}Could not install dependencies${NC}\n" || exit 1
+elif [ -x "$(command -v pacman)" ]; then
+  printf "${GREEN}Installing dependencies for arch based distro${NC}\n"
+  sudo pacman -Syu --noconfirm || printf "${RED}Could not update package list${NC}\n" || exit 1
+  sudo pacman -S cmake make gcc gtkmm3 sqlite --noconfirm || printf "${RED}Could not install dependencies${NC}\n" || exit 1
+elif [ -x "$(command -v yum)" ]; then
+  printf "${GREEN}Installing dependencies with yum${NC}\n"
+  sudo yum update || printf "${RED}Could not update package list${NC}\n" || exit 1
+  sudo yum install cmake make gcc-c++ gtkmm3.0-devel sqlite-devel || printf "${RED}Could not install dependencies${NC}\n" || exit 1
 else
-    printf "${RED}Unsupported distro, please open a github issue or pull-request${NC}\n"
-    exit 1
+  printf "${RED}Unsupported package manager, please open a github issue or a pull request${NC}\n"
+  exit 1
 fi
 
 if [ ! -d "cmake-build-release" ]; then
